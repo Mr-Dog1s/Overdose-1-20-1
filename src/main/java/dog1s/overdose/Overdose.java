8bcf822d10c40c3f7b1aeebfd2940cd5f1ab3aed
Mr-Dog1s <91806076+Mr-Dog1s@users.noreply.github.com>
2026-04-23 00:50:14 +0200

    Itemclass shenanigans

--- src/main/java/dog1s/overdose/Overdose.java
+++ src/main/java/dog1s/overdose/Overdose.java
@@ -1,10 +1,16 @@
 package dog1s.overdose;
 
+import dog1s.overdose.item.ModItems;
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Overdose implements ModInitializer {
+        public static final String MOD_ID = "overdose";
+        public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
+        ModItems.registerModItem();
     }
 }
---
+++ src/main/java/dog1s/overdose/item/ModItems.java
@@ -0,0 +1,32 @@
+package dog1s.overdose.item;
+
+import dog1s.overdose.Overdose;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static final Item SALT = registerItem("salt", new Item(new FabricItemSettings()));
+
+    private static void addItemGroup(FabricItemGroupEntries entries){
+        entries.add(SALT);
+    }
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, new Identifier(Overdose.MOD_ID, name), item);
+    }
+
+    public static void  registerModItem(){
+        Overdose.LOGGER.info("Registering Mod Items for +" + Overdose.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemGroup);
+    }
+
+}
