bd72a7ece4ce9c1973f4f321f8b925ce854b6e4f
Mr-Dog1s <91806076+Mr-Dog1s@users.noreply.github.com>
2026-04-26 12:44:14 +0200

    dryer but shit

---
+++ src/main/java/dog1s/overdose/block/custom/DryerBlock.java
@@ -0,0 +1,10 @@
+package dog1s.overdose.block.custom;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+
+public class DryerBlock extends Block {
+    public DryerBlock(Block.Settings settings){
+        super(settings);
+    }
+}
